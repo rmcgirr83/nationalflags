fcbd27bd056f3f95124cdee92517ca3126961d43
Richard McGirr <rmcgirr83@gmail.com>
2015-08-10 06:08:30 -0400

    Fix encoding issue

--- language/en/common.php
+++ language/en/common.php
@@ -31,7 +31,7 @@ if (empty($lang) || !is_array($lang))
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 // Some characters you may want to copy&paste:
-// ’ » “ ” …
+// â€™ Â» â€œ â€ â€¦
 
 $lang = array_merge($lang, array(
 	'FLAG'				=> '%s flag',
